b9a4eb6b05e1cc3588fd6e8bfc2a92d2cba297fb
cfunch29 <cfunch29@gmail.com>
2025-12-02 17:58:03 -0500

    success in part1 2 and 3. need help with . didn't get to 4

---
+++ script.js
@@ -0,0 +1,31 @@
+// The initial numbers that must be verified.
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+// Check if all numbers are divisible by 5. Cache the result in a variable.
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+const isDivisibleBy5 = `n1 / 5 + n2 / 5 + n3 / 5 + n4 / 5`;
+console.log(`The four numbers are valid according to the provided criteria: ${isDivisibleBy5}.`);
+// can't figure out how to run so that each expression is interpreted
+// I tried (||) , (+), parentheses, and backtic all results are the same even when I change the variables
+
+// Check if the first number is larger than the last. Cache the result in a variable.
+console.log(n1 > n4);
+
+// Accomplish the following arithmetic chain:
+// Subtract the first number from the second number.
+// Multiply the result by the third number.
+// Find the remainder of dividing the result by the fourth number.
+
+const Subtract = n2 - n1;
+console.log(Subtract);
+
+const multiply = Subtract * n3;
+console.log(multiply);
+
+const remainder = multiply / n4;
+console.log(remainder)
+
